f0f82e32c58dd5e3df2e02fb04c57700fe03a55d
testname <test>
2025-01-23 18:40:53 +0900

    no-sandbox

--- other_projects/wasm_project_test/puppeteer.js
+++ other_projects/wasm_project_test/puppeteer.js
@@ -18,7 +18,9 @@ function slp(milliseconds) {
 
     
     
-    const browser = await puppeteer.launch({});
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
 
     let okOnce = false
