47f0ae3c4c50487b40dead18870c67fcef070ef4
nagano1 <nagano1@users.noreply.github.com>
2026-05-09 16:39:45 +0900

    try to fix linking

--- src/main.cpp
+++ src/main.cpp
@@ -32,5 +32,101 @@ fn Main()
     int ret = ScriptEnv::startScript(source);
     printf("ret = %d", ret);
 
+{
+                    auto *text = const_cast<char *>(u8R"(
+class fjawioejap
+{
+    fn funcB()
+    {
+        let a = 893214
+        let *str = "0jfoiwjoie"
+        int ab = 123412
+        "fjoiiw" // u889u98u
+
+
+
+        $float f = 4503
+        ?let *f = null
+        $let g = true
+        
+        "abcdefg"
+        3142
+        true
+        null
+        false
+
+        "jfiowjaoie"
+        123490
+        false
+        null
+        true
+        /*
+        ofijaweifaow 
+        aiwjeofijaw jpawijfw4213
+        jioawjeifoaw
+        jfaiwejfiwoajoijw
+        jofijeiowajiwa
+        */
+        // joiwfaejfo
+        // joijweoifjaw oiaoiwjfoijwaioji
+
+        let *abc = "joifwjoe01234"
+        let f = 343214213
+        int a = 3124
+        2134
+        4123
+
+        afwfiiaeawe
+        taijetjwap
+        faijefjwap
+        true
+        "jofoajwieofawo"
+    }
+}
+
+
+
+
+class A
+{
+    class AClass/**/
+    {/**/
+        // awef
+        fn ffanc()
+        {
+            3142314
+            false
+            // jfoiaweoifaw
+            
+            func(true, "jfoiw", 1203)
+            
+            return/*true*/3241//面白すぎ
+            return/**/21241
+            false//tugi ga saigono kyokudesu
+            return 1
+
+        }
+
+
+        fn a()
+        {
+            // afjiowe
+
+        } // afweo
+    } // joiwafjoefwa 
+    //
+    /* fwaei */
+}
+
+
+        )");
+                auto *document = Alloc::newDocument(DocumentType::CodeDocument, nullptr);
+                DocumentUtils::parseText(document, text, strlen(text));
+                char *treeText = DocumentUtils::getTextFromTree(document);
+
+                free(treeText);
+                Alloc::deleteDocument(document);
+        }
+
     return 0;
 }
