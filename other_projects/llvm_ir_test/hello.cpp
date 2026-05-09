3c4765740001c6233f208d104edf6d28c90412f1
nagano1 <nagano1@users.noreply.github.com>
2026-05-09 13:52:39 +0900

    remove other_projects

--- other_projects/llvm_ir_test/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "lib.h"
-
-int main(int argc, char **argv) {
-    int i = 242;
-    printf("Hello, world!%d \n", i);
-    return func(4);
-}
--- other_projects/llvm_ir_test/lib.h
+++
@@ -1,4 +0,0 @@
-//#include <stdio.h>
-
-int func(int argc);
-int fib(int n);
