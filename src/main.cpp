8d4deae2fbcec471c9c13dff4606429c551e1118
nagano1 <nagano1@users.noreply.github.com>
2026-05-09 16:37:06 +0900

    add test to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,36 @@
 #include <cstdio>
 
+#include "code_nodes.hpp"
+#include "parse_util.hpp"
+#include "script_runtime.hpp"
+
+
 bool stopped = false;
 int wakeup_count = 0;
 
+using namespace smart;
+
 int main()
 {
-    printf("DoorLang");
+    printf("cshort");
     fflush(stdout);
 
     //exit(1);
     //assert(false);
+
+    constexpr char source[] = R"(
+fn Main()
+{
+    int b = 1
+    int a = 1
+    int c = -9
+    
+    return c - (b - a)
+}
+)";
+    printf("%s", source);
+    int ret = ScriptEnv::startScript(source);
+    printf("ret = %d", ret);
 
     return 0;
 }
