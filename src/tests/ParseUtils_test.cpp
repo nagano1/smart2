d2fd8058c5cfc4f5d862d541e1e188fae9dcde72
Your Name <you@example.com>
2026-05-15 17:29:54 +0900

    parser_syntax_test

--- src/tests/ParseUtils_test.cpp
+++ src/tests/ParseUtils_test.cpp
@@ -65,7 +65,7 @@ int testA() {
     {
         std::string class_text(u8"     \tclassauto * 😂日本語=10234;");
         int index = ParseUtil::matchAt(class_text.c_str(), class_text.length(), 0, "class");
-        assert(7 == index);
+        assert(6 == index);
     }
 
 
