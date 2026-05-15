655362b90f59b9684c15d78c737fac2840e9a52c
Your Name <you@example.com>
2026-05-15 17:24:41 +0900

    test 3

--- src/tests/ParseUtils_test.cpp
+++ src/tests/ParseUtils_test.cpp
@@ -37,7 +37,6 @@ int testA() {
 
     }
 
-    return 0;
 
     {
         static constexpr char chars[] = "return    \n";
@@ -62,6 +61,7 @@ int testA() {
     constexpr char txt[] = "aefvariable aowef \n";
     assert(-1 == ParseUtil::matchAt(txt, sizeof(txt)-1, 2, "false"));
 
+    return 0;
 
     {
         std::string class_text(u8"     \tclassauto * 😂日本語=10234;");
