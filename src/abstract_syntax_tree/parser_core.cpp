60a0a4a95218d2603efff97d941fa95a35d2f41b
nagano1 <nagano1@users.noreply.github.com>
2026-05-18 07:01:15 +0900

    scanEnd fix 3

--- src/abstract_syntax_tree/parser_core.cpp
+++ src/abstract_syntax_tree/parser_core.cpp
@@ -324,6 +324,7 @@ namespace smart
 
             if (result == Search::DONE_WITH_PREVIUS_POSITION) {
                 returnResult = context->prevFoundPos;
+                context->scanEnd = false;
                 break;
             }
 
@@ -355,6 +356,7 @@ namespace smart
                 lastLineBreak = nullptr;
 
                 if (context->scanEnd) {
+                    context->scanEnd = false;
                     break;
                 }
 
