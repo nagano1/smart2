b3ae212a98eb194cea71144677369e356b1d29af
nagano1 <nagano1@users.noreply.github.com>
2026-05-18 06:02:54 +0900

    fail test

--- src/abstract_syntax_tree/nodes/AssignStatement.cpp
+++ src/abstract_syntax_tree/nodes/AssignStatement.cpp
@@ -151,7 +151,7 @@ namespace smart {
                 if (assignment->hasTypeDecl) {
                     context->setCodeNode(nullptr);
                     context->scanEnd = true;
-                    return Search::DONE_WITH_SAME_POSITION;//start;// context->prevFoundPos;// assignment->nameNode.found;
+                    return start;// Search::DONE_WITH_SAME_POSITION;//start;// context->prevFoundPos;// assignment->nameNode.found;
                 }
                 //else {
                     //context->scanEnd = true;
