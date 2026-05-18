4e1bc88f2680cd438754e790bc0473a61c1bd602
nagano1 <nagano1@users.noreply.github.com>
2026-05-18 12:40:34 +0900

    woops

--- src/tests/syntax_valid_test.cpp
+++ src/tests/syntax_valid_test.cpp
@@ -188,7 +188,7 @@ void testParsing()
     FUNC(text);
     FUNC(testCode3);
     FUNC(testCode4);
-    FUNC(commentTestText);
+    //FUNC(commentTestText);
     FUNC(parenthesisText);
     FUNC(""); // empty text
     FUNC(" \r\n \n\n  ");
