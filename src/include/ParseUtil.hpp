653ace1509f0e6b48d0ba1719b23156bb3f24eff
Your Name <you@example.com>
2026-05-21 14:58:14 +0900

    revert tab

--- src/include/ParseUtil.hpp
+++ src/include/ParseUtil.hpp
@@ -149,7 +149,7 @@ struct ParseUtil {
 
     static inline bool isSpace(utf8byte ch)
     {
-        return ' ' == ch || '\t' == ch;
+        return ' ' == ch/* || '\t' == ch*/;
     }
 
     static inline bool endsWith(const char* chars, int charsLength, const char* suffix, int suffixLength)
