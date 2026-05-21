1ba30039a44d0da974da8bec828c4fb68b82db53
Your Name <you@example.com>
2026-05-21 15:28:09 +0900

    test

--- src/abstract_syntax_tree/nodes/NameNode.cpp
+++ src/abstract_syntax_tree/nodes/NameNode.cpp
@@ -49,7 +49,7 @@ namespace smart {
         if (found_count > 0) {
             // ban keywords
             constexpr char* keywords[] = {"return", "class", "fn"};
-            for (auto keyword : keywords) {
+            for (auto &&keyword : keywords) {
                 if (ParseUtil::matchWord(context->chars, context->length, keyword, st_size_of(keyword) - 1, start)) {
                     return Search::NOTFOUND;
                 }
