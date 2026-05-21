460a37f26a084923d4454d8c9b674d97dba863c3
Your Name <you@example.com>
2026-05-21 13:24:32 +0900

    refactor _matchFirstWithTrim

--- src/utils/ParseUtil.cpp
+++ src/utils/ParseUtil.cpp
@@ -151,42 +151,45 @@ int ParseUtil::utf16_length(const char *utf8_chars, unsigned int byte_len) {
 // EXPECT_EQ(0, Tokenizer::matchAt("class A{}", "class"));
 int ParseUtil::_matchFirstWithTrim(const char *chars, int charsLength, const char *target, int start)
 {
-    //  return -1 if it fails
     int currentTargetIndex = 0;
     int matchStartIndex = -1;
 
     for (int i = start; true; i++) {
         auto ch = chars[i];
 
-        if ((ch & 0x80) != 0x80) {
-            if (ch == '\0') {
-                return -1;
-            }
+        // use & and != 0x80 to determine if it's a ascii char, since the target should be ascii char.
+        // ascii char has 0 in the most significant bit, while non-ascii char has 1 in the most significant bit,
+        // so we can use & with 0x80 to determine if it's ascii char or not
+        if ((ch & 0x80) == 0x80) {
+            return -1;
+        }
 
-            if (matchStartIndex == -1) {
-                if (ch == ' ') { //allow trim
-                    continue;
-                }
-                else if (ch == '\t' || ch == '\n' || ch == '\r') {
-                    continue;
-                }
-                else { // should start match
-                    matchStartIndex = i;
-                    currentTargetIndex = 0;
-                }
-            }
+        if (ch == '\0') { // end of chars, match failed
+            return -1;
+        }
 
-            if (target[currentTargetIndex] == '\0') {
-                break;
-            }
-
-            if (target[currentTargetIndex] == chars[i]) {
-                currentTargetIndex++;
+        if (matchStartIndex == -1) {
+            if (ch == ' ') { //allow trim
                 continue;
+            }
+            else if (ch == '\t' || ch == '\n' || ch == '\r') { // allow trim
+                continue;
+            }
+            else { // first non-trim char, determine if it's the first char of target
+                matchStartIndex = i;
+                currentTargetIndex = 0;
             }
         }
 
-        return -1;
+        if (target[currentTargetIndex] == '\0') {
+            // match success, but need to ensure the char after target is terminatable char
+            break;
+        }
+
+        assert(matchStartIndex != -1);
+        if (target[currentTargetIndex] == chars[i]) {
+            currentTargetIndex++;
+        }
     }
 
     if (currentTargetIndex == 0) {
