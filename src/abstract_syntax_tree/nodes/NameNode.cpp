42b21368a6968e2f36c802a86bd1ac90caedf7e2
nagano1 <nagano1@users.noreply.github.com>
2026-05-23 16:18:47 +0900

    test2

--- src/abstract_syntax_tree/nodes/NameNode.cpp
+++ src/abstract_syntax_tree/nodes/NameNode.cpp
@@ -47,11 +47,9 @@ namespace smart {
         }
 
         if (found_count > 0) {
-            /*
             if (ParseUtil::IsKeyword(context->chars + start, found_count)) {
                 return Search::NOTFOUND;
             }
-*/
             auto *nameNode = Cast::downcast<NameNodeStruct *>(parent);
 
             context->setCodeNode(nameNode);
