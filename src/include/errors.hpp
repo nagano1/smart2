cfe6278a9f97d66645a1f96f581f081a449a4d6f
nagano1 <nagano1@users.noreply.github.com>
2026-05-27 08:57:46 +0900

    success

--- src/include/errors.hpp
+++ src/include/errors.hpp
@@ -99,7 +99,7 @@ namespace smart {
             if (tempList[i].errorCode >= tempList[i + 1].errorCode) { // check if error code is sorted in ascending order, and there is no duplicate error code.
                 return false;
             }
-            if (static_cast<int>(tempList[i].errorIndex) != index) { // check if error index is continuous and starts from 0
+            if (static_cast<int>(tempList[i].errorIndex) != index++) { // check if error index is continuous and starts from 0
                 return false;
             }
         }
